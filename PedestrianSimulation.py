c385ad632af9c8fd99896d047697b28f5701aa02
Sam Hollenbach <shollenb@macalester.edu>
2017-11-29 15:13:11 -0600

    Threw stuff into functions

--- PedestrianSimulation.py
+++ PedestrianSimulation.py
@@ -1,8 +1,10 @@
-
+import numpy
+from scipy import integrate
 
 class Pedestrian:
 
     def __init__(self, x, y):
+        self.m = 1.0
         self.x = x
         self.y = y
         self.vx = 0.0
@@ -16,24 +18,31 @@ class Pedestrian:
         self.x += self.vx*dt
         self.y += self.vy*dt
 
-T = 1000
-t = 1000
-dt = 0.1
-N = 10
-peds = []
-pedRad = 0.2
 
 #Centered at 0, walls will go from -20 to +20
 wallXLength = 40
 wallYLength = 40
 target = (0, 20)
 
+# Makes N pedestrians evenly distributed
+def create_pedestrians(N,radius):
+    peds = []
+    for i in range(N):
+        peds.append(Pedestrian(-2*N*radius + 4*i*radius, -wallYLength/2 + 2*radius))
+    return peds
 
-for i in range(N):
-    peds.append(Pedestrian(-2*N*pedRad + 4*i*pedRad, -wallYLength/2 + 2*pedRad))
+# Start main simulation
+def run():
+    T = 1000
+    t = 1000
+    dt = 0.1
+    N = 10
+    pedRad = 0.2
+    peds = create_pedestrians(N, pedRad)
 
-while t <= T:
-    t += dt
+    while t <= T:
+        t += dt
+        for p in peds:
+            print(p.x)
 
-    for p in peds:
-        print(p.x)
+run()
